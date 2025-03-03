da0e0ffb9966e62d9c9999f960172a407ba3e35b
mostacho2000 <124900152+mostacho2000@users.noreply.github.com>
2025-03-03 02:08:48 -0600

    Listos los menus

---
+++ Assets/Scripts/MenuControler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControler : MonoBehaviour
+{
+    bool cambio;
+
+    public void ChangeScene(string name)
+    {
+        StartCoroutine(Time(name));
+
+    }
+
+    public void Salir()
+    {
+    
+        Debug.Log("Salir...");
+        Application.Quit();
+    }
+    IEnumerator Time(string nameScene)
+    {
+     
+        yield return new WaitForSeconds(0.4f);
+        SceneManager.LoadScene(nameScene);
+    }
+    public void inicio()
+    {
+        SceneManager.LoadScene("GameScene");
+        Cursor.visible = cambio;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void Final()
+    {
+        SceneManager.LoadScene("winner");
+        Cursor.visible = cambio;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
